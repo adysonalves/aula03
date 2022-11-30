5b01ad1c25dfdb6bd300d0264db1d895dc53612b
adysonalves <adyson6@gmail.com>
2022-11-30 20:28:41 -0300

    Finalizando Atividade

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res) => {
-    res.send('Olá mundo!')
+    res.send('Olá mundo!');
+    res.end();
 });
+
+app.get('/retorno', (req,res) => {
+    res.send('NodeJS é muito Fácil!');
+    res.end();
+});
+
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
